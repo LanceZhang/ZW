18ccf3a673dfeeb30454cac0ec28f0ee7273960e
lancezhang <lancezhang@live.com>
2017-03-03 17:53:20 +0800

    add

---
+++ ZYWC.ZW/ZYWC.ZW.Core.Analysis/Model/QianYiGong.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZYWC.ZW.Core.Analysis.Data;
+
+namespace ZYWC.ZW.Core.Analysis.Model
+{
+    public class QianYiGong : BasicGong
+    {
+        public override GongIndex Name
+        {
+            get { return GongIndex.迁移宫; }
+        }
+    }
+}
